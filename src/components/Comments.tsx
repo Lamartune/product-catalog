c44b8c30a6bb31859aadd7543689b5c15912aca3
Lamartune <fatih.tun.2002@gmail.com>
2024-06-30 12:36:15 +0300

    update: improved css

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -11,36 +11,49 @@ const Comments: React.FC<CommentsProps> = ({ productId }) => {
   const [text, setText] = useState("");
 
   const handleAddComment = () => {
-    addComment(productId, username, text);
-    setUsername("");
-    setText("");
+    if (username && text) {
+      addComment(productId, username, text);
+      setUsername("");
+      setText("");
+    }
   };
 
   return (
-    <div>
-      <h2>Comments</h2>
-      <ul>
+    <div className="mt-6">
+      <h2 className="text-2xl font-bold mb-4">Comments</h2>
+      <ul className="mb-4">
         {comments
           .filter((comment) => comment.productId === productId)
           .map((comment, index) => (
-            <li key={index}>
-              <strong>{comment.username}</strong>
-              <p>{comment.text}</p>
+            <li key={index} className="mb-2 p-4 bg-gray-100 rounded-lg">
+              <strong className="block text-lg font-semibold">
+                {comment.username}
+              </strong>
+              <p className="text-gray-700">{comment.text}</p>
             </li>
           ))}
       </ul>
-      <input
-        type="text"
-        placeholder="Your name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <textarea
-        placeholder="Your comment"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleAddComment}>Add Comment</button>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Your name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="w-full p-2 mb-2 border border-gray-300 rounded-lg"
+        />
+        <textarea
+          placeholder="Your comment"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-full p-2 mb-2 border border-gray-300 rounded-lg"
+        />
+        <button
+          onClick={handleAddComment}
+          className="w-full py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300"
+        >
+          Add Comment
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { products } from "../data/product";
 import Comments from "./Comments";
+import { useCart } from "../context/CartContext";
+import { useFavorites } from "../context/FavoritesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 
 const ProductDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const { toggleCart, isInCart } = useCart();
+  const { toggleFavorite, isFavorite } = useFavorites();
 
   if (!id) {
     return <h2>Product not found</h2>;
@@ -18,21 +25,43 @@ const ProductDetail: React.FC = () => {
   }
 
   return (
-    <div>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <img src={product.image} alt={product.title} />
-            </td>
-            <td>
-              <h1>{product.title}</h1>
-            </td>
-          </tr>
-        </tbody>
-        <p>{product.description}</p>
-        <Comments productId={product.id} />
-      </table>
+    <div className="container mx-auto p-4">
+      <div className="bg-white shadow-md rounded-lg p-6">
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:w-1/2 flex justify-center items-center max-h-[900px]">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full rounded-lg object-contain h-full"
+            />
+          </div>
+          <div className="lg:w-1/2 lg:pl-8">
+            <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
+            <p className="text-gray-700 mb-4">{product.description}</p>
+            <div>
+              <button
+                onClick={() => toggleCart(product)}
+                className={`px-10 py-2 rounded  text-white hover:scale-105 ${
+                  isInCart(product.id) ? "bg-red-500" : "bg-[#EE964B]"
+                }`}
+              >
+                {isInCart(product.id) ? "Remove from Cart" : "Add to Cart"}
+              </button>
+
+              <span className=" mb-2 mr-2 px-4 py-2 rounded  text-white">
+                <FontAwesomeIcon
+                  onClick={() => toggleFavorite(product)}
+                  icon={isFavorite(product.id) ? solidHeart : regularHeart}
+                  className={`text-2xl hover:cursor-pointer ${
+                    isFavorite(product.id) ? "text-red-500" : "text-black"
+                  } hover:scale-125`}
+                />
+              </span>
+            </div>
+            <Comments productId={product.id} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
